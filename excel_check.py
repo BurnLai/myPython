9b2e3a91bcce2a0b8ce9c04c173f268e1e30b43f
BurnLai <71087349+BurnLai@users.noreply.github.com>
2021-03-14 18:41:54 +0800

    Add files via upload

---
+++ excel_check.py
@@ -0,0 +1,53 @@
+import openpyxl
+
+# load excel file
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+# dictionary of suppliers and kinds of products
+product_per_supplier = {}
+
+# dictionary of total values per supplier
+total_value_per_supplier = {}
+
+# check inventory under 10
+product_under_10_inv = {}
+
+# count the rows of the sheet and put in (product_list_max_row)
+for product_row in range(2, product_list.max_row+1):
+
+    # 4 is to get the supplier
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
+
+
+    # calculate number of products per suppliers
+    if supplier_name in product_per_supplier:
+        current_num_products = product_per_supplier.get(supplier_name)
+        product_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        # print("adding a new supplier")
+        product_per_supplier[supplier_name] = 1
+
+    # calculate total values of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+    # logic products with inventory under 10
+    if inventory < 10:
+        product_under_10_inv[product_num] = inventory
+
+    # add value for inventory price
+    inventory_price.value = inventory * price
+
+print(product_per_supplier)
+print(total_value_per_supplier)
+print(product_under_10_inv)
+
+inv_file.save("inventory_valued.xlsx")
